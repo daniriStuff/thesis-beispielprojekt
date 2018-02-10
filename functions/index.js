3688a760a92162aa263dfe72714d616125ebc211
dinodanio <rieser.daniel@gmail.com>
2018-02-10 11:14:21 +0100

    v0.1 branch init

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')({origin: true})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
         response.status(200).send("Das ist Version 0.1");
