6d04216becec7ec2d051dd04470a466012df7648
dinodanio <rieser.daniel@gmail.com>
2018-02-25 13:58:42 +0100

    test

--- functions/index.js
+++ functions/index.js
@@ -5,19 +5,20 @@ const cors = require('cors')({ origin: true })
  * CRUD-Operationen werden unterstuetzt
  */
 exports.exampleFunction = functions.https.onRequest((request, response) => {
-    // Die Zahlenfolge vom request abfangen und zuweisen
-    let zf = JSON.parse(request.body).zahlenfolge
-    // Neues temporaeres Array welches danach die Zahlen sortiert
-    let zfA = []
-    // Die Zahlenfolge als string zu einem Array
-    for (var i = 0; i < zf.length; i++) {
-        zfA.push(zf.charAt(i))
-    }
-    // Sortieren der Zahlen
-    zfA.sort()
+
 
     // Wird fuer Access-Cross-Platform benoetigt
     cors(request, response, () => {
+        // Die Zahlenfolge vom request abfangen und zuweisen
+        let zf = JSON.parse(request.body).zahlenfolge
+        // Neues temporaeres Array welches danach die Zahlen sortiert
+        let zfA = []
+        // Die Zahlenfolge als string zu einem Array
+        for (var i = 0; i < zf.length; i++) {
+            zfA.push(zf.charAt(i))
+        }
+        // Sortieren der Zahlen
+        zfA.sort()
         // Response zurueck zum client mit einem sortiertem Array
         response.status(200).send(JSON.stringify(zfA))
     })
